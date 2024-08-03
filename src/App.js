2855e221ddc7943527e66216ee5b6a2388513308
Narasimha <123463333+Narasimha76@users.noreply.github.com>
2024-08-03 18:43:12 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Narasimha swamy.
+          Narasimha swamy
         </p>
         <a
           className="App-link"
