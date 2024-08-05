4426788863e15d5b3aee78072f65b2d784d2026c
Narasimha <123463333+Narasimha76@users.noreply.github.com>
2024-08-05 18:45:22 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Kubernetes
+          Hello Namaste 
         </p>
         <a
           className="App-link"
@@ -21,5 +21,4 @@ function App() {
     </div>
   );
 }
-
 export default App;
